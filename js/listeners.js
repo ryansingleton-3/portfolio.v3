c4933959cccbfd53e84e21d93ab5e3e8491bf471
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-06-21 19:39:47 -0400

    optimized for mobile and desktop

--- js/listeners.js
+++ js/listeners.js
@@ -82,6 +82,74 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
+
+let menuButton = document.querySelector('.hamburger-menu');
+let navMenu = document.querySelector('#right-side-nav');
+
+menuButton.addEventListener('click', function() {
+    navMenu.classList.toggle('active');
+    if (navMenu.classList.contains("active")) {
+        menuButton.style.position = "fixed";
+        menuButton.style.top = "10px";
+        menuButton.style.right = "10px";
+    } else {
+        menuButton.style.position = "";
+        menuButton.style.top = "";
+    }
+});
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 768) {
+        navMenu.classList.remove('active'); // Hide the navigation menu
+        menuButton.style.position = ""; // Reset the position of the hamburger menu
+        menuButton.style.top = "";
+        menuButton.style.right = "";
+    }
+});
+  
+// Function to hide or show social icons
+function checkScroll() {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        // If screen width is larger than 768px, always show social icons
+        socialIcons.forEach(icon => {
+            icon.style.opacity = '1';
+            icon.style.visibility = 'visible';
+        });
+    } else {
+        // If screen width is less than 768px, hide/show social icons based on scroll position
+        if (window.pageYOffset > 100) {
+            // If page is scrolled, hide social icons
+            socialIcons.forEach(icon => {
+                icon.style.opacity = '0';
+                icon.style.visibility = 'hidden';
+            });
+        } else {
+            // If page is not scrolled, show social icons
+            socialIcons.forEach(icon => {
+                icon.style.opacity = '1';
+                icon.style.visibility = 'visible';
+            });
+        }
+    }   
+}
+
+
+// Run checkScroll function when page is scrolled
+window.onscroll = checkScroll;
+
+
+// Check if screen size is greater than 768px
+if (window.matchMedia("(min-width: 768px)").matches) {
+    // If media query matches
+    var hamburger = document.querySelector(".hamburger-menu");
+    hamburger.classList.remove("active");
+  } else {
+    // If media query does not match
+    var element = document.querySelector(".hamburger-menu");
+    element.classList.add("active");
+  }
+  
+
   
   
   
